0762cdb497e61174b505506b34bab765fae1874d
MatStarJ <kibe1121@naver.com>
2022-07-22 01:27:31 +0900

    ah..

--- src/main/java/com/matstar/shop/controller/CartController.java
+++ src/main/java/com/matstar/shop/controller/CartController.java
@@ -1,16 +1,17 @@
 package com.matstar.shop.controller;
 
+import com.matstar.shop.dto.CartDetailDto;
 import com.matstar.shop.dto.CartItemDto;
+import com.matstar.shop.dto.CartOrderDto;
 import com.matstar.shop.service.CartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.security.Principal;
@@ -20,11 +21,23 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CartController {
 
+    @GetMapping("/cart")
+    public String orderHist(Principal principal, Model model) {
+
+        //로그인한 사용자의 이메일 정보를 이용하여 장바구니에 담겨있는 상품 정보 조회
+        List<CartDetailDto> cartDetailList =
+                cartService.getCartList(principal.getName());
+
+        model.addAttribute("cartItems",cartDetailList);
+
+        return "cart/cartList";
+    }
+
+
     private final CartService cartService;
 
     @PostMapping(value = "/cart")
-    public @ResponseBody
-    ResponseEntity order(@RequestBody @Valid CartItemDto cartItemDto, BindingResult bindingResult, Principal principal){
+    public @ResponseBody ResponseEntity order(@RequestBody @Valid CartItemDto cartItemDto, BindingResult bindingResult, Principal principal){
 
         if(bindingResult.hasErrors()){
             StringBuilder sb = new StringBuilder();
@@ -48,5 +61,54 @@ public class CartController {
 
         return new ResponseEntity<Long>(cartItemId, HttpStatus.OK);
     }
+
+
+    @PatchMapping("/cartItem/{cartItemId}")
+    public @ResponseBody ResponseEntity updateCartItem(@PathVariable("cartItemId") Long cartItemId, int count, Principal principal) {
+
+        if(count <= 0 ) {
+            return new ResponseEntity<String>("최소 1개 이상 담아주세요",HttpStatus.BAD_REQUEST);
+
+        }else if(!cartService.validateCartItem(cartItemId,principal.getName())){
+
+            return new ResponseEntity<String>("수정 권한이 없습니다.",HttpStatus.FORBIDDEN);
+        }
+
+        cartService.updateCartItemCount(cartItemId,count);
+        return new ResponseEntity<Long>(cartItemId,HttpStatus.OK);
+
+    }
+
+    @DeleteMapping("/cartItem/{cartItemId}")
+    public @ResponseBody ResponseEntity deleteCartItem(@PathVariable("cartItemId") Long cartItemId, Principal principal) {
+
+        if(!cartService.validateCartItem(cartItemId, principal.getName())){
+            return new ResponseEntity<String>("수정 권한이 없습니다.",HttpStatus.FORBIDDEN);
+        }
+
+        cartService.deleteCartItem(cartItemId);
+        return new ResponseEntity<Long>(cartItemId,HttpStatus.OK);
+    }
+
+
+    @PostMapping(value = "/cart/orders")
+    public @ResponseBody ResponseEntity orderCartItem(@RequestBody CartOrderDto cartOrderDto, Principal principal){
+
+        List<CartOrderDto> cartOrderDtoList = cartOrderDto.getCartOrderDtoList();
+
+        if(cartOrderDtoList == null || cartOrderDtoList.size() == 0){
+            return new ResponseEntity<String>("주문할 상품을 선택해주세요", HttpStatus.FORBIDDEN);
+        }
+
+        for (CartOrderDto cartOrder : cartOrderDtoList) {
+            if(!cartService.validateCartItem(cartOrder.getCartItemId(), principal.getName())){
+                return new ResponseEntity<String>("주문 권한이 없습니다.", HttpStatus.FORBIDDEN);
+            }
+        }
+
+        Long orderId = cartService.orderCartItem(cartOrderDtoList, principal.getName());
+        return new ResponseEntity<Long>(orderId, HttpStatus.OK);
+    }
+
 
 }
--- src/main/java/com/matstar/shop/repository/CartItemRepository.java
+++ src/main/java/com/matstar/shop/repository/CartItemRepository.java
@@ -1,11 +1,27 @@
 package com.matstar.shop.repository;
 
-
+import com.matstar.shop.dto.CartDetailDto;
 import com.matstar.shop.entity.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CartItemRepository extends JpaRepository<CartItem,Long> {
 
     CartItem findByCartIdAndItemId(Long cartId, Long itemId);
+
+    // 장바구니 페이지에 전달할 CartDetailDto 리스트를 조회하는 쿼리문 작성
+    // CartDetailDto의 생성자를 이용하여 DTO를 반환할 때는 new 키워드와 함께 패키지와 클래스 명을 적어준다.
+    // 또한 생성자의 파라미터 순서를 지켜야 한다.
+@Query("select new com.matstar.shop.dto.CartDetailDto(ci.id, i.itemNm, i.price, ci.count, im.imgUrl) " +
+        "from CartItem ci, ItemImg im " +
+        "join ci.item i " +
+        "where ci.cart.id = :cartId " +
+        "and im.item.id = ci.item.id " +
+        "and im.repimgYn = 'Y' " +
+        "order by ci.regTime desc"
+)
+List<CartDetailDto> findCartDetailDtoList(Long cartId);
 
 }
